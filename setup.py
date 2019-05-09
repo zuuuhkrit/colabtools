5ecd3e5492c9702bc615583881fd5f8c591b24b4
zuuuhkrit <49794641+zuuuhkrit@users.noreply.github.com>
2019-05-09 15:56:36 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="colabtools",
+    version="0.0.1",
+    author="zuuuhkrit",
+    author_email="zuuuhkrit@gmail.com",
+    description="Tools to work with colab from google",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zuuuhkrit/colabtools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPL-3.0",
+        "Operating System :: OS Independent",
+    ],
+)
